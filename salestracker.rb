0b5e05d561e9bad84a4b1d2cefb990e1b0131e1a
Jam Creencia <jrcreencia@gmail.com>
2016-07-30 13:23:15 +1000

    Added profit margin, modified print

--- salestracker.rb
+++ salestracker.rb
@@ -4,13 +4,17 @@ class SalesTracker
 
 	def initialize
 		@count = 0
-		@product_charges = 0
-		@giveaway_rebates = 0
-		@promos = 0
+		@gross_sales = 0
+		@promo_rebates = 0
+		@promo_units = 0
 		@promo_cost = 0
-		@units_sold = 0
+		@units_ordered = 0
 		@amazon_fees = 0
 		@num_of_returns = 0
+		@production_cost_per_unit = 4.65
+		@inventory_placement_fee = 0
+		@other = 0
+		@advertising_fee = 0
 	end
 
 	def count(filename)
@@ -20,8 +24,8 @@ class SalesTracker
 
 		  if @count >= 5
 		  	if row[3].include?('Order Payment') && row[4].include?('Product charges')
-			  	@product_charges += convert_to_f(row[6])
-			  	@units_sold += row[7].to_i
+			  	@gross_sales += convert_to_f(row[6])
+			  	@units_ordered += row[7].to_i
 			  end
 
 			  if row[3].include?('Order Payment') && row[4].include?('Amazon fees')
@@ -29,7 +33,7 @@ class SalesTracker
 			  end
 
 			  if row[3].include?('Refund') && row[4].include?('Product charges')
-			  	@product_charges += convert_to_f(row[6])
+			  	@gross_sales += convert_to_f(row[6])
 			  	@num_of_returns += row[7].to_i
 			  end
 
@@ -39,44 +43,77 @@ class SalesTracker
 
 				if row[3].include?('Order Payment') && row[4].include?('Promo rebates')
 					rebate = convert_to_f(row[6])
- 			  	@giveaway_rebates += rebate
+ 			  	@promo_rebates += rebate
 
  			  	if rebate < -15
-			  		@promos += 1 
+			  		@promo_units += 1 
 			  	end
 			  end
 
-			  if row[3].include?('Order Payment') && row[4].include?('Other')
-			  	@product_charges += convert_to_f(row[6])
+			  if row[3].include?('Order Payment') && row[4].include?('Other') && row[5].include?('Shipping')
+			  	@other += convert_to_f(row[6])
 			  end
+
+			  if row[3].include?('Service Fees') && row[4].include?('Amazon fees') && row[5].include?('FBA Inventory Placement Service Fee')
+			  	@inventory_placement_fee += convert_to_f(row[6])
+			  end
+
+				if row[3].include?('Service Fees') && row[4].include?('Transaction Details') && row[5].include?('Cost of Advertising')
+			  	@advertising_fee += convert_to_f(row[6])
+			  end
+			  
 		  end		  
 		end
 		
-		print	
+		print
 	end
 
 	def print
 		puts "--------------------------------"
 		puts "Report as of #{Date.today}"
 		puts "--------------------------------"
-		puts "giveaway units: #{@promos}"
-		puts "full price units(incl. returns): #{full_price_units_sold - @num_of_returns}"
+		puts "Units given away: #{@promo_units}"
+		puts "Units full price: #{full_price_units_sold - @num_of_returns}"
 		puts ""
-		puts "units sold: #{@units_sold}"
-		puts "units returned: #{@num_of_returns}"
-		puts "total units sold: #{total_units_sold}"
+		puts "Units ordered: #{@units_ordered}"
+		puts "Units returned: #{@num_of_returns}"
 		puts ""
-		puts "total inventory stock: #{inventory_stock}"
+		puts "Total units sold: #{total_units_sold}"
+		puts "Total inventory stock: #{inventory_stock}"
 		puts "--------------------------------"
-		puts "charges: #{@product_charges.round(2)}"
-		puts "giveaway rebates: #{@giveaway_rebates.round(2)}"
-		puts "amazon fees: #{@amazon_fees.round(2)}"
+		puts "Gross sales: #{gross_sales}"
+		puts "Amazon fees: #{amazon_fees}"
+		puts "Promo rebates: #{promo_rebates}"
+		puts "Other: #{@other}"
+		puts "Inventory placement: #{@inventory_placement_fee}"
+		puts "Advertising fee: #{advertising_fee}"
+		puts "********************************"
+		puts "PAYOUT: #{amazon_payout}"
+		puts "********************************"
+		puts "Product costs: #{cost_of_goods_sold}"
 		puts "--------------------------------"
-		puts "total amazon profit: #{total_profit.round(2)}"
-		puts "actual profit per box: #{actual_profit_per_box.round(2)}"
-		puts "total actual profit: #{(actual_profit_per_box * full_price_units_sold).round(2)}"	
+		puts "PROFIT: #{gross_profit}"
+		puts "Margin: #{gross_profit_margin}%"
 		puts "--------------------------------"
+		puts "Units to sell to break even: #{units_to_breakeven}"
 		puts ""
+		
+	end
+
+	def gross_sales
+		@gross_sales.round(2)
+	end
+
+	def amazon_fees
+		@amazon_fees.round(2)
+	end
+
+	def promo_rebates
+		@promo_rebates.round(2)
+	end
+
+	def advertising_fee
+		@advertising_fee.round(2)
 	end
 
 	def convert_to_f(price_str)
@@ -84,16 +121,20 @@ class SalesTracker
 		price_str.to_f
 	end
 
-	def total_profit
-		@product_charges + @amazon_fees + @giveaway_rebates
+	def units_to_breakeven
+		((5544.15 - amazon_payout) / 16.95).round
+	end
+
+	def amazon_payout
+		(@gross_sales + @amazon_fees + @promo_rebates + @other + @inventory_placement_fee + @advertising_fee).round(2)
 	end
 
 	def actual_profit_per_box
-		(total_profit / full_price_units_sold) - 4.65
+		(amazon_payout / full_price_units_sold) - @production_cost_per_unit
 	end
 
 	def full_price_units_sold
-		@units_sold - @promos
+		@units_ordered - @promo_units
 	end
 
 	def inventory_stock
@@ -101,7 +142,19 @@ class SalesTracker
 	end
 
 	def total_units_sold
-		@units_sold - @num_of_returns
+		@units_ordered - @num_of_returns
+	end
+
+	def gross_profit
+		amazon_payout - cost_of_goods_sold
+	end
+
+	def gross_profit_margin
+		((gross_profit / amazon_payout) * 100).round(2)
+	end
+
+	def cost_of_goods_sold
+		@production_cost_per_unit * total_units_sold
 	end
 
 end
@@ -109,4 +162,5 @@ end
 
 sales_tracker = SalesTracker.new
 sales_tracker.count('report.csv')
+
 
